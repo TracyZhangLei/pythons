8ba578727efb82935ef1e76961938a5ce8125cfd
tracy <760796714@qq.com>
2016-08-30 16:33:46 +0800

    update tinyPics.py: auto record compressed file list && add white list config

--- tinyPics.py
+++ tinyPics.py
@@ -9,7 +9,7 @@ import os
 #===============tinypng python scripts for android=================== 
 
 
-tinify.key = 'your key'
+#tinify.key = 'your key'
 
 def get_picpaths(directory):
     file_paths = []  # List which will store all of the full filepaths.
@@ -18,10 +18,23 @@ def get_picpaths(directory):
 	for filename in files:
 	    # Join the two strings in order to form the full filepath.
 	    filepath = os.path.join(root, filename)
-            if 'drawable' in filepath and 'intermediates' not in filepath and '.9.png' not in filepath:
+            if 'drawable' in filepath and not in_compressed_list(filepath) and 'intermediates' not in filepath and '.9.png' not in filepath:
 	      file_paths.append(filepath)
    
     return file_paths
+
+def in_compressed_list(filePath):
+    re = filePath in open('compressed_file_list.txt').read()
+    if re:
+	print filepath+" in compressed_file_list"
+    return re
+
+def append_compressed_file(filePath):
+    fp = open("compressed_file_list.txt","a+")
+    fp.write(filePath+"\n")
+    fp.flush()
+    fp.close()
+
 
 full_pic_paths = get_picpaths(os.getcwd())
 
@@ -31,4 +44,5 @@ for f in full_pic_paths:
     for i in types:
       if i in f:
         print "tinypng "+f
-        tinify.from_file(f).to_file(f)
+#        tinify.from_file(f).to_file(f)
+        append_compressed_file(f)
